c6da21f70c090089e8ce226390ca6320fa4d9851
Zhian N. Kamvar <zkamvar@gmail.com>
2020-09-28 10:28:04 -0700

    it might work this time

--- episodes/extras/deploy.R
+++ episodes/extras/deploy.R
@@ -1,11 +1,12 @@
-branch <- "gh-pages"
-remote <- "origin"
-commit_message <- "semi-automated update of gh-pages"
-git    <- pkgdown:::git
-
-dest_dir <- fs::dir_create(fs::file_temp())
-on.exit(fs::dir_delete(dest_dir))
-if (!pkgdown:::git_has_remote_branch(remote, branch)) {
+gh_pages <- function(
+  branch = "gh-pages",
+  remote = "origin",
+  commit_message = "semi-automated update of gh-pages"
+  ) {
+  git    <- pkgdown:::git
+  dest_dir <- fs::dir_create(fs::file_temp())
+  on.exit(fs::dir_delete(dest_dir))
+  if (!pkgdown:::git_has_remote_branch(remote, branch)) {
     old_branch <- pkgdown:::git_current_branch()
     git("checkout", "--orphan", branch)
     git("rm", "-rf", "--quiet", ".")
@@ -13,12 +14,14 @@ if (!pkgdown:::git_has_remote_branch(remote, branch)) {
         branch))
     git("push", remote, paste0("HEAD:", branch))
     git("checkout", old_branch)
+  }
+  git("remote", "set-branches", remote, branch)
+  git("fetch", remote, branch)
+  pkgdown:::github_worktree_add(dest_dir, remote, branch)
+  on.exit(pkgdown:::github_worktree_remove(dest_dir), add = TRUE)
+  sandpaper::build_lesson(here::here(), override = list(destination = dest_dir))
+  pkg <- pkgdown::as_pkgdown(here::here("site"), override = list(destination = dest_dir))
+  pkgdown:::build_github_pages(pkg)
+  pkgdown:::github_push(dest_dir, commit_message, remote, branch)
 }
-git("remote", "set-branches", remote, branch)
-git("fetch", remote, branch)
-pkgdown:::github_worktree_add(dest_dir, remote, branch)
-on.exit(pkgdown:::github_worktree_remove(dest_dir), add = TRUE)
-sandpaper::build_lesson(here::here(), override = list(destination = dest_dir))
-pkg <- pkgdown::as_pkgdown(here::here("site"), override = list(destination = dest_dir))
-pkgdown:::build_github_pages(pkg)
-pkgdown:::github_push(dest_dir, commit_message, remote, branch)
+gh_pages()
